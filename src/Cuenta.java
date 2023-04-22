246bacf77788a2d19e1e4cc64f9a3ef8ffdb7321
Grincode <juanpetarcich@gmail.com>
2023-04-22 01:18:30 -0300

    Creacion de test
    Se agregan metodos para ingresar, sacar o transferir dinero

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,7 +1,32 @@
-public class Cuenta{
+public class Cuenta {
 
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+	public void deposita(double valor) {
+
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfiere(double valor, Cuenta destino) {
+
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 }
